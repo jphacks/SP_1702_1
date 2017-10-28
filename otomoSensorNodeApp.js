23240481de456021e8d1519b829e15d9e61373ff
Koki Watanabe <gateway0616@gmail.com>
2017-10-28 19:01:25 +0900

    [add] シリアルポートからjson受取る

--- otomoSensorNodeApp.js
+++ otomoSensorNodeApp.js
@@ -14,3 +14,19 @@ var sp = new serialport.SerialPort(portName, {
   flowControl: false,
   parser: serialport.parsers.readline("\n")
 });
+
+
+// data from Serial port
+sp.on('data', function(input) {
+
+    var buffer = new Buffer(input, 'utf8');
+    var jsonData;
+    try {
+        jsonData = JSON.parse(buffer);
+        console.log('temp: ' + jsonData.temp);
+        console.log('led: ' + jsonData.led);
+    } catch(e) {
+        return;
+    }
+    io.sockets.json.emit('message', { value: jsonData });
+});
