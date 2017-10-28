f0972d0a42446f65de86b25b3d234dc8a0cfa9d8
Koki Watanabe <gateway0616@gmail.com>
2017-10-28 19:00:27 +0900

    [modify] シリアルポート再定義

--- otomoSensorNodeApp.js
+++ otomoSensorNodeApp.js
@@ -1,15 +1,16 @@
-var serialport = require('serialport');
+var express = require('express')
+  , routes = require('./routes')
+  , path = require('path')
+  , serialport = require('serialport');
 
-// -----------------------
-// --------- 中略 ---------
-// -----------------------
 
-var portName = "/dev/cu.usbmodem1421";
+// Serial Port
+var portName = '/dev/cu.usbmodem1421';
 var sp = new serialport.SerialPort(portName, {
-    baudRate: 9600,
-    dataBits: 8,
-    parity: 'none',
-    stopBits: 1,
-    flowControl: false,
-    parser: serialport.parsers.readline("\n")
+  baudRate: 9600,
+  dataBits: 8,
+  parity: 'none',
+  stopBits: 1,
+  flowControl: false,
+  parser: serialport.parsers.readline("\n")
 });
